f6ba5b3f7e7eb62cd7070a5417ee1f11cb7155aa
Anartz Mugika <mugan86@gmail.com>
2015-09-10 11:56:04 +0200

    Update login.html url to open

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,7 @@ angular.module('euroku.controllers', [])
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+  $ionicModal.fromTemplateUrl('templates/user/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
