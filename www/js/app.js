a177647ce7b81141cd29946b7d568613a01dc6c3
Anartz Mugika <mugan86@gmail.com>
2015-09-21 10:46:53 +0200

    Start from playlists

--- www/js/app.js
+++ www/js/app.js
@@ -162,5 +162,5 @@ angular.module('euroku', ['ionic',
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/main');
+  $urlRouterProvider.otherwise('/app/playlists');
 });
