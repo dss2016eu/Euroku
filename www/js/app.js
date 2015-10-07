a806283591f7ff6502e9b9a3f830410e686abe0e
Anartz Mugika <mugan86@gmail.com>
2015-10-07 11:38:15 +0200

    Set main in app.js (default state)

--- www/js/app.js
+++ www/js/app.js
@@ -173,5 +173,5 @@ angular.module('euroku', ['ionic',
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/main');
 });
