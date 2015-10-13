569a8ebdbc5fc2ea4a6d9365110c4833ce9a65b0
Anartz Mugika <mugan86@gmail.com>
2015-10-13 08:58:37 +0200

    Remove unused states references and controllers

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,4 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('euroku', ['ionic',
                           'ngCordova' ,
                           'euroku.controllers',
@@ -51,8 +46,6 @@ angular.module('euroku', ['ionic',
   {
     console.log(window.localStorage.getItem('lang'));
   }
-
-
 })
 
 
@@ -116,15 +109,6 @@ angular.module('euroku', ['ionic',
     }
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
   .state('app.quiz', {
     url: '/quiz',
     views: {
@@ -163,35 +147,7 @@ angular.module('euroku', ['ionic',
           controller: 'SettingsCtrl'
         }
       }
-    })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+    });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/main');
 });
