693adf288eb8938fcc1e62115fd08e983fe32714
Anartz Mugika <mugan86@gmail.com>
2015-12-21 12:20:00 +0100

    GetQUestions data and send question after response question service

---
+++ www/js/services/questions.js
@@ -0,0 +1,45 @@
+/**********************************************************************************************
+Url Services fitxategia:
+----------------------------
+
+
+***********************************************************************************************/
+
+
+angular.module('euroku.services.questions', [])
+
+
+.service('questionsServices', function questionsServices($http, URL_LOCALHOST, QUESTIONS) {
+
+    return {
+      getQuestion : function (game_id)
+      {
+        if (game_id !== "" && game_id !== undefined)
+        {
+          game_id = "&game_id=" + game_id;
+        }
+        var device_id = "1";
+
+        return $http.get(URL_LOCALHOST+QUESTIONS+"?device_id=" + device_id + game_id).success(
+
+            function(resp)
+            {
+              return resp.data;
+            }
+        );
+      },
+      setQuestionRequest: function(params)
+      {
+
+        return $http.post(URL_LOCALHOST + QUESTIONS, params).success(
+
+            function(data) {
+
+              console.log("Send user answer...");
+              return data;
+            }
+        );
+      }
+      //gehiago gehitu daitezke...
+    };
+  });
