cfe6fe106e7cbb4b7518c46b610b65bf9d92f156
Anartz Mugika <mugan86@gmail.com>
2015-12-21 13:16:23 +0100

    Add new constants and update

--- www/js/constants.js
+++ www/js/constants.js
@@ -8,5 +8,6 @@ Url Services fitxategia:
 
 angular.module('euroku.constants', [])
 
-
-.constant('URL_LOCALHOST', 'http://euroku.eus/');
+.constant('URL_LOCALHOST', 'http://arno.cs:8000/')
+.constant('QUESTIONS', 'api/1.0/galdera');
+//.constant('URL_LOCALHOST', 'http://euroku.eus/');
