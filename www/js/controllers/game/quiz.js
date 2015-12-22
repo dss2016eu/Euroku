547273d3f03049269f7cd0ab6fea4fb024bbc8a8
Anartz Mugika <mugan86@gmail.com>
2015-12-22 12:06:20 +0100

    Add chronometer and manage logic to show in view

--- www/js/controllers/game/quiz.js
+++ www/js/controllers/game/quiz.js
@@ -17,19 +17,78 @@ angular.module('euroku.quiz', [])
 
   $scope.loading = true;
 
-
+  $rootScope.counter = 10;
   $rootScope.menu_show= false;
 
   $ionicSideMenuDelegate.canDragContent(false);
 
+
+  /****************************************************************************
+    CHRONOMETER
+  *****************************************************************************/
+
+  $scope.question_game = {};
+
+  $rootScope.show_time = "00:10";
+
+
+
+  var mytimeout = null; // the current timeoutID
+
+  // actual timer method, counts down every second, stops on zero
+  $scope.onTimeout = function() {
+      if($rootScope.counter ===  0) {
+          $scope.$broadcast('timer-stopped', 0);
+          $rootScope.counter = 10;
+          $timeout.cancel(mytimeout);
+          return;
+      }
+      $rootScope.counter --;
+      console.log($rootScope.counter);
+
+      $scope.show_time = "00:0" + $rootScope.counter;
+      mytimeout = $timeout($scope.onTimeout, 1000);
+  };
+
+    $scope.startTimer = function(question) {
+      $scope.question_game = question;
+      console.log($scope.question_game);
+      $rootScope.counter = 10;
+      mytimeout = $timeout($scope.onTimeout, 1000);
+      console.log("138 line: " + $scope.question_game.game_id);
+
+    };
+
+    // stops and resets the current timer
+    $scope.stopTimer = function() {
+        $scope.$broadcast('timer-stopped', $scope.counter);
+        $rootScope.counter = 10;
+        console.log($scope.counter);
+        $timeout.cancel(mytimeout);
+    };
+
+    // triggered, when the timer stops, you can do something here, maybe show a visual indicator or vibrate the device
+    $scope.$on('timer-stopped', function(event, remaining) {
+        if(remaining === 0) {
+            console.log('your time ran out!');
+            $rootScope.show_time = "00:10";
+            $rootScope.counter = 10;
+            $state.go('app.result', { choose: -1, game_id: $scope.question_game.game_id, question_id: $scope.question_game.id});
+            $ionicHistory.nextViewOptions({
+              disableBack: true
+            });
+        }
+    });
+
   $scope.sendOption = function (option)
   {
-    console.log(option);
     $scope.stopTimer();
-    $state.go('app.result', { 'choose_option': option});
+    console.log(option);
+    $state.go('app.result', { choose: option, game_id: $scope.question_game.game_id, question_id: $scope.question_game.id});
     $ionicHistory.nextViewOptions({
       disableBack: true
     });
+
   };
 
   $scope.showVideo = function ()
@@ -88,7 +147,7 @@ angular.module('euroku.quiz', [])
           $scope.question = resp.data;
           console.log($scope.question);
 
-          $scope.startTimer();
+          $scope.startTimer($scope.question);
           //console.log($scope.question);
         },
         function(error)
@@ -99,12 +158,13 @@ angular.module('euroku.quiz', [])
                               answer_one: "A erantzuna",
                               answer_two: "B erantzuna",
                               answer_three: "C erantzuna",
-                              title: "Euroku",
+                              title: "Euroku (Question)",
                               photo: "",
+                              game_id: 25,
                               id: 10
                             };
                             console.log($scope.question);
-                            $scope.startTimer();
+                            $scope.startTimer($scope.question);
         });
 
 });
