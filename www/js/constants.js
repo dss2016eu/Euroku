898b1fe5897357655a5cf14c75e0f73bec30a9e9
Anartz Mugika <mugan86@gmail.com>
2015-12-22 09:38:49 +0100

    Change URL_LOCALHOST in constant file

--- www/js/constants.js
+++ www/js/constants.js
@@ -8,6 +8,6 @@ Url Services fitxategia:
 
 angular.module('euroku.constants', [])
 
-.constant('URL_LOCALHOST', 'http://arno.cs:8000/')
+.constant('URL_LOCALHOST', 'http://eurokuserver.cs/')
 .constant('QUESTIONS', 'api/1.0/galdera');
 //.constant('URL_LOCALHOST', 'http://euroku.eus/');
