34e70ce829ced73f603f065ea811dc86f680d0b8
Anartz Mugika <mugan86@gmail.com>
2015-12-23 09:47:55 +0100

    Constant values update with PRICE constant different URLs

--- www/js/constants.js
+++ www/js/constants.js
@@ -9,5 +9,10 @@ Url Services fitxategia:
 angular.module('euroku.constants', [])
 
 .constant('URL_LOCALHOST', 'http://eurokuserver.cs/')
-.constant('QUESTIONS', 'api/1.0/galdera');
-//.constant('URL_LOCALHOST', 'http://euroku.eus/');
+.constant('QUESTIONS', 'api/1.0/galdera')
+.constant('PROFILE', 'api/1.0/profile')
+.constant('PRICES', {
+                        list: "api/1.0/prices",
+                        detail_item: "api/1.0/price/",
+                        public_list: "api/1.0/prices/public"
+                    });
