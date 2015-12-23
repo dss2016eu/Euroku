75b4be12a5cfe264f376ac328cdf942a82498a55
Anartz Mugika <mugan86@gmail.com>
2015-12-23 12:18:43 +0100

    Manage profile controller

---
+++ www/js/controllers/user/profile.js
@@ -0,0 +1,30 @@
+angular.module('euroku.profile', [])
+.controller('SettingsCtrl', function($scope, $ionicHistory, $state, profileServices) {
+  $scope.answer_correct = getRandomInt(1,16); //get a number in range 1-16
+  $scope.total_answer = 16;
+
+  $scope.progress = ($scope.answer_correct * 100) / $scope.total_answer;
+
+  profileServices.getDetails ()
+        .then(function(resp)
+        {
+          console.log(resp);
+
+          $scope.profile = resp.data;
+
+          console.log(resp.data);
+
+          $scope.startTimer($scope.question);
+          //console.log($scope.question);
+        },
+        function(error)
+        {
+          console.error("Errorea");
+
+          $scope.profile =  {
+                              language: "eu",
+                              device_id: "1"
+                            };
+          console.log($scope.question);
+        });
+});
