20a158268f8fa5eff97de45bccf748eb904798b6
Anartz Mugika <mugan86@gmail.com>
2015-12-23 09:53:52 +0100

    Add Prices request service

---
+++ www/js/services/prices.js
@@ -0,0 +1,67 @@
+/**********************************************************************************************
+Url Services fitxategia:
+----------------------------
+
+
+***********************************************************************************************/
+
+
+angular.module('euroku.services.prices', [])
+
+
+.service('pricesServices', function pricesServices($http, URL_LOCALHOST, PRICES) {
+
+    return {
+      getUserListPrices : function ()
+      {
+
+        var device_id = "1";
+
+        /********************************************************
+        Load device id from local Storage
+        *********************************************************/
+
+        console.log(URL_LOCALHOST+PRICES.list+"?device_id=" + device_id);
+
+        return $http.get(URL_LOCALHOST+PRICES.list+"?device_id=" + device_id).success(
+
+            function(resp)
+            {
+              return resp.data;
+            }
+        );
+      },
+      getPriceDetails : function ()
+      {
+
+        var device_id = "1";
+
+        /********************************************************
+        Load device id from local Storage
+        *********************************************************/
+
+        console.log(URL_LOCALHOST+PRICES.detail_item+"?device_id=" + device_id);
+
+        return $http.get(URL_LOCALHOST+PRICES.detail_item+"?device_id=" + device_id).success(
+
+            function(resp)
+            {
+              return resp.data;
+            }
+        );
+      },
+      getPublicPriceList: function()
+      {
+        console.log(URL_LOCALHOST+PRICES.public_list);
+        return $http.get(URL_LOCALHOST+PRICES.public_list).success(
+
+            function(data) {
+
+              console.log("Send user answer...");
+              return data;
+            }
+        );
+      }
+      //gehiago gehitu daitezke...
+    };
+  });
