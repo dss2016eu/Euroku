52c85909a1ef95cfbffbee97a160fe8c1ba1a14d
Anartz Mugika <mugan86@gmail.com>
2016-01-07 09:58:26 +0100

    Get Data to Prices from server

--- www/js/controllers/game/prices.js
+++ www/js/controllers/game/prices.js
@@ -12,9 +12,81 @@
 
 angular.module('euroku.prices', [])
 
-.controller('PricesCtrl', function($scope, $rootScope, $ionicSideMenuDelegate) {
+.controller('PricesCtrl', function($scope, $rootScope, $ionicSideMenuDelegate, pricesServices) {
   $rootScope.menu_show = true;
   $ionicSideMenuDelegate.canDragContent(true);
+
+  pricesServices.getUserListPrices()
+        .then(function(resp)
+      {
+        console.log("52: " + resp);
+      },
+      function(error)
+      {
+        console.error(error);
+  });
+
+  pricesServices.getPriceDetails()
+    .then (function(resp)
+      {
+        console.log("52: " + resp);
+      },
+      function(error)
+      {
+        console.error(error);
+  });
+
+
+    pricesServices.getPublicPriceList()
+    .then (function(resp)
+      {
+        console.log("52: " + resp);
+      },
+      function(error)
+      {
+        console.error(error);
+  });
+
+      $scope.prices = [
+                        {
+                          description: "Price description...",
+                          title: "Boligrafoa",
+                          photo: "",
+                          active: true,
+                          id: 129
+                        },
+                        {
+                          description: "Price description...",
+                          title: "Kursaal Sarrerak",
+                          photo: "",
+                          active: false,
+                          id: 130
+                        },
+                        {
+                          description: "Sarrerak hitzaldirako",
+                          title: "Tokialdatu / Desplazamiento",
+                          photo: "",
+                          active: false,
+                          id: 136
+                        },
+                        {
+                          description: "Potentzia handiko linternak eskuan hartuta, 30 bat pertsonako...",
+                          title: "Argi-gerrilla",
+                          photo: "",
+                          active: true,
+                          id: 131
+                        },
+                        {
+                          description: "Price description...",
+                          title: "Feministaldia",
+                          photo: "",
+                          active: true,
+                          id: 133
+                        }
+                    ];
+
+    console.log($scope.prices);
+
 });
 
 
