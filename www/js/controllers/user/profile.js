2c05968db60c5d35e435427c96554b13dc3f6b5a
Anartz Mugika <mugan86@gmail.com>
2016-01-08 14:03:10 +0100

    Update profile controller

--- www/js/controllers/user/profile.js
+++ www/js/controllers/user/profile.js
@@ -1,32 +1,50 @@
 angular.module('euroku.profile', [])
-.controller('SettingsCtrl', function($scope, $ionicHistory, $state, profileServices) {
+.controller('SettingsCtrl', function($scope, $ionicHistory, $state, profileServices, $ionicLoading) {
   $scope.answer_correct = getRandomInt(1,16); //get a number in range 1-16
   $scope.total_answer = 16;
 
   $scope.progress = ($scope.answer_correct * 100) / $scope.total_answer;
+  $ionicLoading.show();
 
   profileServices.getDetails ()
         .then(function(resp)
-        {
-          console.log(resp);
+  {
+    console.log(resp);
 
-          $scope.profile = resp.data;
+    $scope.profile = resp.data;
 
-          console.log(resp.data);
+    console.log(resp.data);
 
-          $scope.startTimer($scope.question);
-          //console.log($scope.question);
-        },
-        function(error)
-        {
-          console.error("Errorea");
+    $ionicLoading.hide();
+  },
+  function(error)
+  {
+    console.error("Errorea");
 
-          $scope.profile =  {
-                              language: "eu",
-                              device_id: "1"
-                            };
-          console.log($scope.question);
-        });
+    $scope.profile =  {
+                        language: "eu",
+                        device_id: "1"
+                      };
+    console.log($scope.question);
+    $ionicLoading.hide();
+  });
+
+  $scope.savePreferences = function()
+  {
+    var language = window.localStorage.getItem('lang');
+    console.log(language);
+
+    profileServices.setDetails(language)
+          .then(function(resp)
+    {
+      console.log("Profile Controller (38): " + resp);
+    },
+    function(error)
+    {
+      console.error(error);
+    });
+
+  };
 
 
 
