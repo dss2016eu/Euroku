502b599cfe583f985d6d84e1eedd38a55a7ee0e6
Anartz Mugika <mugan86@gmail.com>
2016-01-18 08:11:31 +0100

    Update with new constant

--- www/js/constants.js
+++ www/js/constants.js
@@ -12,6 +12,7 @@ angular.module('euroku.constants', [])
 .constant('URL_LOCALHOST', 'https://euroku.codesyntax.com/')
 .constant('QUESTIONS', 'api/1.0/galdera')
 .constant('PROFILE', 'api/1.0/profile')
+.constant('REGISTER', 'api/1.0/register')
 .constant('PRICES', {
                         list: "api/1.0/prices",
                         detail_item: "api/1.0/price/",
