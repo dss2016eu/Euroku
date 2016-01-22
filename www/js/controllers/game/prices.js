53eeb88fe8044e84495421d69bba05dc4536084d
Anartz Mugika <mugan86@gmail.com>
2016-01-22 10:16:25 +0100

    Manage prices data from server and show in layout

--- www/js/controllers/game/prices.js
+++ www/js/controllers/game/prices.js
@@ -17,15 +17,78 @@ angular.module('euroku.prices', [])
   $ionicSideMenuDelegate.canDragContent(true);
 
   //Erabiltzailearen sariak
-  pricesServices.getUserListPrices()
-        .then(function(resp)
+  pricesServices.getUserListPrices().then(function(resp)
+  {
+    console.log("52: " + resp.data);
+    $scope.user_prices = resp.data;
+    //Dauden sari guztien zerrenda
+    pricesServices.getPublicPriceList().then (function(resp)
+    {
+      console.log("45: " + resp.data);
+
+      $scope.public_prices = [];
+
+      for (var i = 0; i < resp.data.length; i++)
       {
-        console.log("52: " + resp);
-      },
-      function(error)
-      {
-        console.error(error);
-  });
+        var prices = {
+                      amount: resp.data[i].amount,
+                      title: resp.data[i].title,
+                      enddate: resp.data[i].enddate
+                    };
+                    console.log(prices);
+        $scope.public_prices.push(prices);
+      }
+      console.log($scope.public_prices);
+
+    },
+        function(error)
+        {
+          console.error(error);
+          $scope.public_prices = [
+                          {
+                            amount: "Price description...",
+                            title: "Boligrafoa",
+                            enddate: "2016-02-25",
+                            key: "AJUEH2910",
+                            calimed: true
+                          },
+                          {
+                            amount: "Price description...",
+                            title: "Boligrafoa",
+                            enddate: "2016-02-10",
+                            key: "AJUEH2110",
+                            calimed: true
+                          },
+                          {
+                            amount: "Sarrerak hitzaldirako",
+                            title: "Tokialdia / Desplazamiento",
+                            enddate: "2016-01-25",
+                            key: "ALOEH2910",
+                            calimed: true
+                          },
+                          {
+                            amount: "Potentzia handiko linternak eskuan hartuta, 30 bat pertsonako...",
+                            title: "Argi gerrilla",
+                            enddate: "2016-02-25",
+                            key: "AJUEJ0910",
+                            calimed: false
+                          },
+                          {
+                            amount: "Price description...",
+                            title: "Feministaldia",
+                            enddate: "2016-03-30",
+                            calimed: false,
+                            key: "AJDHEWU920"
+                          }
+                      ];
+
+          console.log($scope.public_prices);
+    });
+    },
+    function(error)
+    {
+      console.error(error);
+    });
 
   //Lortutako sariaren xehetasunak
   /*pricesServices.getPriceDetails("DSS2016BOLI_1")
@@ -37,74 +100,6 @@ angular.module('euroku.prices', [])
       {
         console.error(error);
   });*/
-
-  //Dauden sari guztien zerrenda
-    pricesServices.getPublicPriceList()
-    .then (function(resp)
-      {
-        console.log("52: " + resp);
-
-        $scope.user_prices = [];
-
-        for (var i = 0; i < resp.length; i++)
-        {
-          var prices = {
-                        amount: resp.data[i].amount,
-                        title: resp.data[i].title,
-                        enddate: resp.data[i].enddate
-                      };
-                      console.log(prices);
-          $scope.user_prices.push(prices);
-        }
-        console.log($scope.user_prices);
-
-      },
-      function(error)
-      {
-        console.error(error);
-        $scope.user_prices = [
-                        {
-                          amount: "Price description...",
-                          title: "Boligrafoa",
-                          enddate: "2016-02-25",
-                          key: "AJUEH2910",
-                          calimed: true
-                        },
-                        {
-                          amount: "Price description...",
-                          title: "Boligrafoa",
-                          enddate: "2016-02-10",
-                          key: "AJUEH2110",
-                          calimed: true
-                        },
-                        {
-                          amount: "Sarrerak hitzaldirako",
-                          title: "Tokialdia / Desplazamiento",
-                          enddate: "2016-01-25",
-                          key: "ALOEH2910",
-                          calimed: true
-                        },
-                        {
-                          amount: "Potentzia handiko linternak eskuan hartuta, 30 bat pertsonako...",
-                          title: "Argi gerrilla",
-                          enddate: "2016-02-25",
-                          key: "AJUEJ0910",
-                          calimed: false
-                        },
-                        {
-                          amount: "Price description...",
-                          title: "Feministaldia",
-                          enddate: "2016-03-30",
-                          calimed: false,
-                          key: "AJDHEWU920"
-                        }
-                    ];
-
-        console.log($scope.user_prices);
-  });
-
-
-
 });
 
 
